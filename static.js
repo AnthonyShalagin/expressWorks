7ab5174c6ecfcf6380e41d6c0f407836c8d35180
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-04-10 17:11:13 -0400

    static -exercise 2

--- static.js
+++ static.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/home', function(req, res) {
-	res.end('Hello World!');
-});
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
 
 app.listen(process.argv[2]);
